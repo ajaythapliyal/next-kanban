c9c0411e8acfa56bc1f20a60368283821a3904ff
ajaythapliyal <ajaythapliyal1703@gmail.com>
2023-05-31 09:13:23 +0530

    redirects to 404 when resource not found

--- pages/board/[id].js
+++ pages/board/[id].js
@@ -10,6 +10,9 @@ export default function Board({ boards, boardId }) {
 
 export async function getServerSideProps({ params }) {
   const response = await fetch("http://localhost:3000/boards");
-  const responseJson = await response.json();
-  return { props: { boards: responseJson, boardId: params.id } };
+  const boards = await response.json();
+  if (!boards.find((board) => board.id === Number(params.id))) {
+    return { notFound: true };
+  }
+  return { props: { boards, boardId: params.id } };
 }
